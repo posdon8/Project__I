becf70a87139b0c0b5db3788aa96aa4c43f718f0
TamVu2882 <137530351+posdon8@users.noreply.github.com>
2024-12-15 19:07:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -36,33 +36,49 @@ io.on('connection', (socket) => {
       socket.emit('room-creation-failed', 'Không thể tạo phòng. Vui lòng thử lại.');
       return;
     }
-    rooms[roomId] = { users: [username] };
+    rooms[roomId] = { users: [{username, socketId: socket.id}]};
     socket.join(roomId);
     socket.emit('room-created', roomId); // Gửi lại Room ID cho client
     console.log(`Room created: ${roomId}`);
   });
-
+  socket.on('new-user-stream', (data) => {
+    // Phát thông tin stream của người dùng này đến tất cả những người tham gia khác
+    socket.to(data.roomId).emit('new-user-stream', {
+      username: data.username,
+      stream: data.stream
+    });
+  });
   // Khi tham gia phòng
   socket.on('join-room', ({ roomId, username }) => {
     if (rooms[roomId]) {
-      rooms[roomId].users.push(username);
+      if (rooms[roomId].users.length >= 6) {
+        socket.emit('error', 'Phòng đã đầy!');
+        return;
+      }
+      rooms[roomId].users.push({username, socketId: socket.id});
       socket.join(roomId);
-      io.to(roomId).emit('user-joined', username); // Thông báo cho các người dùng khác
+      io.to(roomId).emit('user-joined', rooms[roomId].users); // Thông báo cho các người dùng khác
       console.log(`${username} joined room ${roomId}`);
     } else {
       socket.emit('error', 'Room not found');
+      console.log(`Invalid Room ID: ${roomId} attempted by ${username}`);
     }
   });
 
   // Khi gửi tin nhắn
   socket.on('send-message', ({ roomId, username, message }) => {
-    console.log("tin nhan dc gui");
+    console.log(`Message from ${username} in room ${roomId}: ${message}`);
+    
     io.to(roomId).emit('receive-message', { username, message });
   });
 
   // Khi ngắt kết nối
   socket.on('disconnect', () => {
     console.log('A user disconnected:', socket.id);
+    for (let roomId in rooms) {
+      rooms[roomId].users = rooms[roomId].users.filter(user => user.socketId !== socket.id);
+      io.to(roomId).emit('user-joined', rooms[roomId].users); // Cập nhật danh sách người dùng
+    }
   });
 });
 
